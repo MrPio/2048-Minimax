3cf6c40e3f2563a438d8489dfe64b797a8a12c21
Valerio Morelli <valeriomorelli50@gmail.com>
2023-12-31 16:32:42 +0100

    Update 2048.pl

--- 2048.pl
+++ 2048.pl
@@ -1,6 +1,6 @@
 % 2048 Minimax ==========================================================
 % The USER plays the MIN role, as such they want to minimize the number
-% of empty tiles. On the contrary, the CPU plays the role of MAX, which 
+% of empty tiles. On the countrary, the CPU plays the role of MAX, which 
 % tries to win the player by filling all the board tiles with numbers
 
 %   -  -  -
@@ -8,22 +8,22 @@
 %  16  2  -
 
 % move(+Pos,-NextPos) returns an admissible move from Pos to NextPos
-move([cpu, play, Board], [user, win, NewBoard]) :- % CPU wins
-    move_check(cpu, Board, NewBoard),
-    vincente(cpu, NewBoard), !.
-move([P1, play, Board], [P2, deadlock, NewBoard]) :- % deadlock
+move([cpu, play, Board], [user, win, NextBoard]) :- % CPU wins
+    move_check(cpu, Board, NextBoard),
+    vincente(cpu, NextBoard), !.
+move([P1, play, Board], [P2, deadlock, NextBoard]) :- % deadlock
     next_player(P1, P2),
-    move_check(P1, Board, NewBoard),
-    deadlock(P1, NewBoard), !.
-move([P1, play, Board], [P2, play, NewBoard]) :- % normal move
+    move_check(P1, Board, NextBoard),
+    deadlock(P1, NextBoard), !.
+move([P1, play, Board], [P2, play, NextBoard]) :- % normal move
     next_player(P1, P2),
-    move_check(P1, Board, NewBoard).
+    move_check(P1, Board, NextBoard).
 
 % move_check(+Player, +Board, -NextBoard)
 % NextBoard is Board with free tile filled by USER
-move_check(P, [0|Bs], [P|Bs]).
-move_check(P, [B|Bs], [B|B2s]) :-
-    move_check(P, Bs, B2s).
+move_check(cpu, [0|T], [2|T]).
+move_check(cpu, [B|T], [B|T2]) :-
+    move_check(cpu, T, T2).
 
 % vincente(+Player, +Board)
 % True if Player win in Board.
